9499526dce3e3a294812f70d8d9ed7d33fa90ed2
Kauã Carvalho <contato.kauacarval@gmail.com>
2024-08-31 03:14:26 -0300

    Desafios resolvidos.

---
+++ 2 - Verifica String/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Digite o seu nome completo: ");
+string nomeCompleto = Console.ReadLine();
+
+// Verifica se o usuário inseriu um nome
+if (!string.IsNullOrEmpty(nomeCompleto))
+{
+    int quantidadeLetrasA = 0;
+    string nomeMaiusculo = nomeCompleto.ToUpper();
+
+    if (nomeMaiusculo.Contains("A"))
+    {
+        Console.WriteLine($"Seu nome contém a letra A");
+
+        // Para cada letra na string alocada em nomeMaiusculo,
+        // verifica se é a Letra A e conta a quantidade de vezes que ela aparece.
+        foreach (char letra in nomeMaiusculo)
+        {
+            if (letra == 'A')
+            {
+                quantidadeLetrasA++;
+            }
+        }
+
+        Console.WriteLine($"A letra A aparece {quantidadeLetrasA} vezes no seu nome");
+    }
+    else
+    {
+        Console.WriteLine("Seu nome não possui a letra A");
+    }
+}
+else
+{
+    Console.WriteLine("O nome não pode estar vazio.");
+}
---
+++ 3 - Desafio SOMA/ProjetoSoma/Program.cs
@@ -0,0 +1,14 @@
+﻿int indice = 12;
+int soma = 0;
+int k = 1;
+
+while (k < indice)
+{
+    k = k + 1;
+    soma = soma + k;
+}
+
+Console.WriteLine(soma);
+
+// A variável soma, 
+// ao final do processamento terá o valor: 77
